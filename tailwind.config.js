53d69361bb0a78da7199a0f43f670222945bfdd2
Naomi Mango <naomi.mango@gmail.com>
2022-02-04 12:53:41 +0100

    [MAJ] Fonctionnalités de TailwindCSS - La personnalisation
    • postcss
    • ajout de styles personnalisés
    • configuration de thèmes
    • ajout de styles personnalisés en réutilisant des classes de Tailwind avec @apply

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
     content: ["./*.html", "./pages/*/*.html"],
     theme: {
-        extend: {},
+        /* Ne garde uniquement que ces styles dans le projet et efface ceux de Tailwind */
+        // colors: {
+        //     customColors: "#000",
+        // },
+
+        /* Ajoute ces styles au projet en plus de ceux de Tailwind */
+        extend: {
+            spacing: {
+                enorme: "900px",
+            },
+            colors: {
+                customColors: "#000",
+            },
+            screens: {
+                customRes: "1000px",
+            },
+        },
     },
     plugins: [],
 };
