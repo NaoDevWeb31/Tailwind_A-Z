90730b90899e3603e33c86434c7985825355acbc
Naomi Mango <naomi.mango@gmail.com>
2022-02-08 12:50:15 +0100

    [MAJ] Création de site avec TailwindCSS - Navbar

--- creation_site/tailwind.config.js
+++ creation_site/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     content: ["./index.html"],
     theme: {
-        extend: {},
+        extend: {
+            minHeight: {
+                500: "500px",
+                650: "650px",
+            },
+        },
     },
     plugins: [],
 };
